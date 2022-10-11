20349d3f5d01e7f3aec8a8784d53a96c2a668a96
AmitKarnam <amitkarnam01@gmail.com>
2022-10-11 15:01:20 +0530

    UI : Added Material UI for header.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -28,6 +28,9 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatNativeDateModule} from '@angular/material/core';
 import {MatButtonModule} from '@angular/material/button';
 import { MatIconModule } from "@angular/material/icon";
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatListModule} from '@angular/material/list';
 @NgModule({
   declarations: [
     AppComponent,
@@ -59,7 +62,10 @@ import { MatIconModule } from "@angular/material/icon";
     MatInputModule,
     MatNativeDateModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatListModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
